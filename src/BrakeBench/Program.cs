a6b0ad52e6f57928093c35d55e379411e62ea7b5
sr55 <sr55.hb@outlook.com>
2020-09-14 19:51:46 +0100

    Initial Project Structure

---
+++ src/BrakeBench/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="HandBrake Project (https://handbrake.fr)">
+//   This file is part of the BrakeBench source code - It may be used under the terms of the 3-Clause BSD License
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BrakeBench
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
